d00b15288ad831c3c083b35767f59afde162fdc7
mobbcitestjob4 <citestjob4@mobb.ai>
2025-06-22 15:07:29 +0000

    Test commit

--- xss-example.js
+++ xss-example.js
@@ -1,7 +1,8 @@
-// This is an intentionaly vulnerable Javascript file used for testing, do not use or deploy it anywhere.
 
-var urlParams = new URLSearchParams(window.location.search);
-var username = urlParams.get('username');
-
-var unsafe_div = window.document.getElementById("vulnerable-div");
-unsafe_div.innerHTML = "Hello to you ";
+<html>
+<head>
+<title>Fastly error: unknown domain raw.githubusercontent.com</title>
+</head>
+<body>
+<p>Fastly error: unknown domain: raw.githubusercontent.com. Please check that this domain has been added to a service.</p>
+<p>Details: cache-dfw-kdfw8210039-DFW (185.199.110.133)</p></body></html>
